e747a819cf943d2ba9757dd7174c74acee5bf469
ramcesbasubas24 <ramcesbasubas@gmail.com>
2022-12-19 17:32:04 +0800

    initial commit

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['I','B']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'contact_number' => $this->faker->phoneNumber(),
+            'address' => $this->faker->streetAddress()
+        ];
+    }
+}
---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $status = $this->faker->randomElement(['B','P','V']);
+        return [
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100,20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL
+
+        ];
+    }
+}
